fb6a3757903e0cb0ee05175f95d5604d42049598
Robert Böhnke <robb@robb.is>
2013-11-16 16:46:36 +0100

    Improve nil-handling of ASTMap

--- Asterism/ASTMap.h
+++ Asterism/ASTMap.h
@@ -12,7 +12,7 @@
 
 // Maps a block across an array.
 //
-// array - An array of elements. This argument must not be nil.
+// array - An array of elements.
 // block - A block that takes an element as its only argument and returns a new
 //         element. The block must not be nil.
 //
@@ -23,7 +23,7 @@ OVERLOADABLE NSArray *ASTMap(NSArray *array, id(^block)(id obj));
 
 // Maps a block across an array.
 //
-// array - An array of elements. This argument must not be nil.
+// array - An array of elements.
 // block - A block that takes an element and its index in `array` as its
 //         arguments and returns a new element. The block must not be nil.
 //
@@ -34,7 +34,7 @@ OVERLOADABLE NSArray *ASTMap(NSArray *array, id(^block)(id obj, NSUInteger idx))
 
 // Maps a block across a dicitonary.
 //
-// dict  - An dictionary of elements. This argument must not be nil.
+// dict  - An dictionary of elements.
 // block - A block that takes a value as its only argument and returns a new
 //         value. The block must not be nil.
 //
@@ -45,7 +45,7 @@ OVERLOADABLE NSDictionary *ASTMap(NSDictionary *dict, id(^block)(id obj));
 
 // Maps a block across a dicitonary.
 //
-// dict  - An dictionary of elements. This argument must not be nil.
+// dict  - An dictionary of elements.
 // block - A block that takes a key and a value as its arguments and returns a
 //         new value. The block must not be nil.
 //
@@ -56,7 +56,7 @@ OVERLOADABLE NSDictionary *ASTMap(NSDictionary *dict, id(^block)(id key, id obj)
 
 // Maps a block across an array.
 //
-// set   - An set of elements. This argument must not be nil.
+// set   - An set of elements.
 // block - A block that takes an element as its only argument and returns a new
 //         element. The block must not be nil.
 //
@@ -67,7 +67,7 @@ OVERLOADABLE NSSet *ASTMap(NSSet *set, id(^block)(id obj));
 
 // Maps a block across an ordered set.
 //
-// set   - An orderd set of elements. This argument must not be nil.
+// set   - An orderd set of elements.
 // block - A block that takes an element as its only argument and returns a new
 //         element. The block must not be nil.
 //
@@ -78,7 +78,7 @@ OVERLOADABLE NSOrderedSet *ASTMap(NSOrderedSet *set, id(^block)(id obj));
 
 // Maps a block across an ordered set.
 //
-// set   - An orderd set of elements. This argument must not be nil.
+// set   - An orderd set of elements.
 // block - A block that takes an element and its index in `set` as its
 //         arguments and returns a new element. The block must not be nil.
 //
