c8e261866d0a388c16144f4ef88abb3c35f81dbf
Robert Böhnke <robb@robb.is>
2013-11-21 00:52:03 +0100

    Add ASTDefaults

---
+++ Asterism/ASTDefaults.h
@@ -0,0 +1,21 @@
+//
+//  ASTDefaults.h
+//  Asterism
+//
+//  Created by Robert Böhnke on 16/11/13.
+//  Copyright (c) 2013 Robert Böhnke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ASTConstants.h"
+
+// Fills in missing values from
+//
+// dict     - A dictionary.
+// defaults - A dictionary with default values.
+//
+// Returns a dictionary that contains a union of key-value-pairs of dict and
+// defaults. Key-value-pairs of dict will have precedence over those taken from
+// defaults.
+OVERLOADABLE NSDictionary *ASTDefaults(NSDictionary *dict, NSDictionary *defaults);
