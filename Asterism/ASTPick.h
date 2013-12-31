b27767d0c8d221799608f22ad9d6be4e237b34bc
Robert Böhnke <robb@robb.is>
2013-12-31 14:39:19 +0100

    Add ASTPick

---
+++ Asterism/ASTPick.h
@@ -0,0 +1,13 @@
+//
+//  ASTPick.h
+//  Asterism
+//
+//  Created by Robert Böhnke on 31/12/13.
+//  Copyright (c) 2013 Robert Böhnke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ASTConstants.h"
+
+OVERLOADABLE NSDictionary *ASTPick(NSDictionary *dict, NSArray *keys);
