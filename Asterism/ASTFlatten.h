92b95c24144b28448edf8fc70ef1567a20526885
Robert Böhnke <robb@robb.is>
2014-03-12 12:12:46 +0100

    Apply static inline to ASTFlatten

--- Asterism/ASTFlatten.h
+++ Asterism/ASTFlatten.h
@@ -8,10 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+// You should not call these methods directly.
+NSArray *__ASTFlatten_NSArray(NSArray *array);
+
 // Flattens an array a single level.
 //
 // array - An array of elements.
 //
 // Returns a new array that concatenates all array elements in `array` while
 // preserving non-array elements.
-NSArray *ASTFlatten(NSArray *array);
+static inline __attribute__((overloadable)) NSArray *ASTFlatten(NSArray *array) {
+    return __ASTFlatten_NSArray(array);
+}
