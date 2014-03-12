6054c934be0a99f2cd4d97b49427157ebde4dca5
Robert Böhnke <robb@robb.is>
2014-03-12 12:22:26 +0100

    Apply static inline to ASTHead

--- Asterism/ASTHead.h
+++ Asterism/ASTHead.h
@@ -8,18 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
-#import "ASTConstants.h"
+// You should not call these methods directly.
+id __ASTHead_NSArray(NSArray *array);
+id __ASTHead_NSOrderedSet(NSOrderedSet *set);
 
 // Returns the first element of an array.
 //
 // array - An array of elements.
 //
 // Returns the first element or `nil` if the array is empty.
-OVERLOADABLE id ASTHead(NSArray *array);
+static inline __attribute__((overloadable)) id ASTHead(NSArray *array) {
+    return __ASTHead_NSArray(array);
+}
 
 // Returns the first element of an ordered set.
 //
 // set - An ordered set of elements.
 //
 // Returns the first element or `nil` if the ordered set is empty.
-OVERLOADABLE id ASTHead(NSOrderedSet *set);
+static inline __attribute__((overloadable)) id ASTHead(NSOrderedSet *set) {
+    return __ASTHead_NSOrderedSet(set);
+}
