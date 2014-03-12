8050bd328898fb608dbb5bb14168560bfc504702
Robert Böhnke <robb@robb.is>
2014-03-12 13:51:45 +0100

    Remove ASTConstants.h

--- Asterism/ASTConstants.h
+++
@@ -1,9 +0,0 @@
-//
-//  ASTConstants.h
-//  Asterism
-//
-//  Created by Robert Böhnke on 6/1/13.
-//  Copyright (c) 2013 Robert Böhnke. All rights reserved.
-//
-
-#define OVERLOADABLE __attribute__((overloadable))
