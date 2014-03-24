9acbafa494c0e748f34a96bdcc4d0674ec3611c0
Robert Böhnke <robb@robb.is>
2014-03-24 17:14:47 +0100

    Update documentation

--- generate.rb
+++ generate.rb
@@ -6,13 +6,13 @@ require 'tomparse'
 docs = Dir["Asterism/Asterism/AST*.h"].map do |filename|
     file = File.read filename
 
-    blocks = file.scan(/((\/\/.*\s)+((.+\;$)|(^\#.+$)))/).map { |c| c[0] }
+    blocks = file.scan(/((\/\/.*\n?)+\nASTERISM_OVERLOADABLE.+) \{/).map { |c| c[0] }
 
     # Parse all tomdoc blocks
     blocks.map do |block|
         signature = block.lines.to_a[-1]
         name = signature.scan(/(AST[\w]+)\(/).flatten[0]
-        tomdoc = TomParse.parse(block.lines.to_a[0..-2].join.gsub(/\/\/?/, ''))
+        tomdoc = TomParse.parse(block.lines.to_a[0..-2].join.gsub(/^\/\/?/, ''))
 
         {
             :name => name,
