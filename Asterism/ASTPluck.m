04e15f80bb080ebe09a811613e70b4768add0671
Robert Böhnke <robb@robb.is>
2014-03-27 13:37:01 +0100

    Add dictionary support to ASTPluck

--- Asterism/ASTPluck.m
+++ Asterism/ASTPluck.m
@@ -6,7 +6,17 @@
 //  Copyright (c) 2013 Robert Böhnke. All rights reserved.
 //
 
+#import "ASTMap.h"
+
 #import "ASTPluck.h"
+
+NSDictionary *__ASTPluck_NSDictionary(NSDictionary *dict, NSString *keyPath) {
+    NSCParameterAssert(keyPath != nil);
+
+    return ASTMap(dict, ^id(id obj) {
+        return [obj valueForKeyPath:keyPath];
+    });
+}
 
 NSArray *__ASTPluck_NSFastEnumeration(id<NSFastEnumeration> collection, NSString *keyPath) {
     NSCParameterAssert(keyPath != nil);
